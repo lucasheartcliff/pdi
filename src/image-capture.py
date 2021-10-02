163a361b43e93f1dc65fde4f4e6cb5dda3fe6d79
Lucas Morais <lucas.morais@intelie.com.br>
2021-10-02 17:54:25 -0300

    adjusting parameters

--- src/image-capture.py
+++ src/image-capture.py
@@ -15,7 +15,12 @@ img = cv.imread(path.abspath('assets/img/005.png'))
 
 gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-faces = face_classifier.detectMultiScale(gray_img, 1.05, 9)
+faces = face_classifier.detectMultiScale(
+    gray_img,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    flags=cv.CASCADE_SCALE_IMAGE
+)
 
 for (x, y, w, h) in faces:
     cv.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
@@ -25,18 +30,27 @@ for (x, y, w, h) in faces:
     face = img[y:y+h, x:x+w]
 
     # Detecting Eyes
-    eyes = eye_classifier.detectMultiScale(gray_face, 1.0001, 2)
+    eyes = eye_classifier.detectMultiScale(
+        gray_face,
+        scaleFactor=1.00003,
+        minNeighbors=1,
+        flags=cv.CASCADE_SCALE_IMAGE
+    )
+
     for (ex, ey, ew, eh) in eyes:
         cv.rectangle(face, (ex, ey), (ex+ew, ey+eh), (0, 0, 255), 2)
 
     # Detecting Mounths
-    mounth = mounth_classifier.detectMultiScale(gray_face, 1.075, 4)
+    mounth = mounth_classifier.detectMultiScale(
+        gray_face,
+        scaleFactor=1.007,
+        minNeighbors=20,
+        flags=cv.CASCADE_SCALE_IMAGE,
+        maxSize=(40,25),
+    )
+
     for (ex, ey, ew, eh) in mounth:
         cv.rectangle(face, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
-
-
-
-
 
 cv.imshow("Result", img)
 cv.waitKey(0)
